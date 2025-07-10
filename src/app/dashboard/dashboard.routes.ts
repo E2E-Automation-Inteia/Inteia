9c7c2cc29fb1327f5bf795cff79d0d38dede25e2
baldurt1992 <andresbaldur92@gmail.com>
2025-07-10 18:58:48 -0500

    table added

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
+import { VinculadoresComponent } from './pages/vinculadores/vinculadores.component';
 
 export const dashboardRoutes: Routes = [
   {
@@ -8,7 +9,9 @@ export const dashboardRoutes: Routes = [
     component: DashboardComponent,
     children: [
       { path: '', component: HomeComponent },
+      { path: 'vinculadores', component: VinculadoresComponent }
 
     ]
-  }
+  },
+
 ];
