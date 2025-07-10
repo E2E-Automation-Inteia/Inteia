7b73cf26cab3f63ef00fec8d022ef36f2d2e363c
kamilo9809 <camilorivera09@outlook.com>
2025-07-10 18:39:09 -0500

    merge events and opportunities

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
+import { EventosComponent } from './pages/eventos/eventos.component';
 
 export const dashboardRoutes: Routes = [
   {
@@ -8,7 +9,7 @@ export const dashboardRoutes: Routes = [
     component: DashboardComponent,
     children: [
       { path: '', component: HomeComponent },
-
+      { path: 'events', component: EventosComponent },      
     ]
   }
 ];
