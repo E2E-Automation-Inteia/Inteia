fc4b42562e63da44bd049a1a274666bd26332ff7
kamilo9809 <camilorivera09@outlook.com>
2025-08-15 20:30:26 -0500

    fix: change env

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false, 
-    apiUrlPrivate: 'https://24eb27144a29.ngrok-free.app/api',
-    apiUrlAuth: 'https://24eb27144a29.ngrok-free.app/api/Auth',
+    apiUrlPrivate: 'https://d4ee61602619.ngrok-free.app/api',
+    apiUrlAuth: 'https://d4ee61602619.ngrok-free.app/api/Auth',
 }
